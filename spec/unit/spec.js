e366181cf963d94d1312a4634b160c47dd523296
Bruno Pedroso <Bruno@macasa.local>
2010-01-27 18:20:51 -0200

    teste passando

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -5,13 +5,17 @@ describe 'Minesweeper'
 	it 'should draw a table for the board'
 
 		htmlElement = $(fixture('teste'))
+		
 		htmlElement.should.have_id 'board'
 		htmlElement.children().should.be_empty
 
-		m = new Minesweeper(htmlElement, 2, 3)
+		m = new Minesweeper(htmlElement)
 		
 		htmlElement.find('table').should.have_length 1
 		
 	end
+
+
+
   end
 end
