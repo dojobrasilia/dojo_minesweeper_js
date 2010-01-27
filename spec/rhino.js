6483e4c3f2be03b3ab33366d451468bc1648f45a
Bruno Pedroso <Bruno@macasa.local>
2010-01-27 18:22:48 -0200

    renomeei o js

--- spec/rhino.js
+++ spec/rhino.js
@@ -6,7 +6,7 @@ load('/Users/Bruno/.gem/ruby/1.8/gems/jspec-3.1.3/lib/jspec.js')
 load('/Users/Bruno/.gem/ruby/1.8/gems/jspec-3.1.3/lib/jspec.xhr.js')
 load('/Users/Bruno/.gem/ruby/1.8/gems/jspec-3.1.3/lib/jspec.jquery.js')
 
-load('lib/yourlib.js')
+load('lib/minesweeper.js')
 load('spec/unit/spec.helper.js')
 
 JSpec
