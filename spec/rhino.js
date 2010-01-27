fd8f1199a5922e8577a3d26dbe48e1a8a4a9f056
Bruno Pedroso <Bruno@macasa.local>
2010-01-27 18:09:41 -0200

    primeiro teste falhando

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,6 +1,11 @@
+load('spec/support/env.js')
+Envjs('spec/fixtures/teste.html')
+load('spec/support/jquery.js')
 
 load('/Users/Bruno/.gem/ruby/1.8/gems/jspec-3.1.3/lib/jspec.js')
 load('/Users/Bruno/.gem/ruby/1.8/gems/jspec-3.1.3/lib/jspec.xhr.js')
+load('/Users/Bruno/.gem/ruby/1.8/gems/jspec-3.1.3/lib/jspec.jquery.js')
+
 load('lib/yourlib.js')
 load('spec/unit/spec.helper.js')
 
--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -3,9 +3,14 @@ describe 'Minesweeper'
   describe 'init'
 
 	it 'should draw a table for the board'
-		htmlElement = $(fixture('teste')).filter("#board")
+
+		htmlElement = $(fixture('teste'))
+		htmlElement.should.have_id 'board'
+		htmlElement.children().should.be_empty
+
 		m = new Minesweeper(htmlElement, 2, 3)
-		$(htmlElement)
+		
+		htmlElement.find('table').should.have_length 1
 		
 	end
   end
